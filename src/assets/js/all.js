a36b713d833453d97254708f512c701ef7712789
pwndex <pwndex42@gmail.com>
2020-10-27 15:58:55 +0500

    refactor: gulpfile & package structure

--- src/assets/js/all.js
+++ src/assets/js/all.js
@@ -1,1 +1,7 @@
+import 'particles.js/particles'
+
+particlesJS.load('particles-js', 'assets/js/particles.json', () => {
+  console.log('callback - particles.js config loaded');
+});
+
 console.log('It works!')
